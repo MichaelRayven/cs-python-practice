16b4ba3a8ad754e3633ffa7db601e96be6637a6d
Michael Rayven <themikerayven@gmail.com>
2024-10-24 23:26:34 +0700

    Task 5: Haiku

--- lab7/task5.py
+++ lab7/task5.py
@@ -1,6 +1,22 @@
-import re
+lines = input("Введите предложение: ").split("/")
 
-sentence = input("Введите предложение: ")
-lines = re.split(r"[/\n]\s*", sentence)
-for i in range(len(lines)):
-    print(lines[i])
+if len(lines) != 3:
+    print("Не хайку. Должно быть 3 строки.")
+else:
+    for i in range(len(lines)):
+        count_vowels = 0
+        for char in lines[i]:
+            if char in "aeiouAEIOUауоиэыяюеёАУОИЭЫЯЮЕЁ":
+                count_vowels += 1
+
+        if (i % 2 == 0):
+            if count_vowels != 5:
+                print("Не хайку.")
+                break
+        else:
+            if count_vowels != 7:
+                print("Не хайку.")
+                break
+    else:
+        print("Хайку!")
+
